6929e0ea68b2ba7bf5c19e9e79ee8669801611bc
Petrik Diák <diak@petrik.hu>
2026-02-09 13:16:45 +0100

    LotttoszamokGeneralasa kész

--- lotto/Program.cs
+++ lotto/Program.cs
@@ -2,9 +2,22 @@
 {
     internal class Program
     {
+        static void LottoszamokGeneralas()
+        {
+            Random rnd = new Random();
+            int[] szamok = new int[5];
+            for (int i = 0; i < szamok.Length; i++) 
+            {
+                szamok[i] = rnd.Next(1, 91);
+            }
+        }
+
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            LottoszamokGeneralas();
         }
     }
 }
